4425e15947fccc9f862c459af0e87523df35b12c
Luis Carrillo <lois@localhost.localdomain>
2024-08-27 21:25:11 -0400

    Rebuild get

--- main.py
+++ main.py
@@ -21,7 +21,19 @@ def asset_files(filepath):
 def error404(error):
 	return 'Nothing here, sorry'
 
-@app.route('/DRE/v1/_/', method='GET')
+@app.route('/DIRECCION_REGIONAL_EDUCACION/v1/', method='GET')
+def get_courses():
+	try:
+		with sqlite3.connect(DB_PATH) as connection:
+			with closing(connection.cursor()) as cursor:
+				cursor.execute("SELECT UPPER([Id]) AS id, UPPER([descripción]) AS nm, (([estados] & 1)=1) AS e FROM [Dirección Regional de Educación]", ())
+				cursor.row_factory = sqlite3.Row
+				ds_ = [dict(r) for r in cursor.fetchall()]
+				return bottle.HTTPResponse(body=json.dumps({'data': ds_}), status=200, headers=MINIMAL_CORS)
+	except sqlite3.OperationalError as e:
+		return bottle.HTTPResponse(body=json.dumps({'msg': str(e)}), status=500)
+
+@app.route('/DIRECCION_REGIONAL_EDUCACION/v1/_/', method='GET')
 def get_courses():
 	try:
 		with sqlite3.connect(DB_PATH) as connection:
